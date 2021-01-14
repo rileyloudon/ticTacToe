db4542e1a80a84715b031af58685e2deb94265bb
Riley Loudon <rileyloudon@icloud.com>
2021-01-13 22:06:42 -0600

    allow symbols to be displayed on screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,45 @@
+// Handle all visual changes:
 const Gameboard = (() => {
-  const board = [];
+  const _board = [];
   const checkBoard = () => {
-    return board;
+    return _board;
   };
+
+  const userMove = (e) => {
+    e.target.classList.contains('game-square')
+      ? _board.includes('X' + e.target.id || 'O' + e.target.id)
+        ? false
+        : displayMove(e)
+      : false;
+  };
+
+  const displayMove = (e) => {
+    clickedSquare = document.getElementById(`${e.target.id}`);
+    clickedSquare.innerHTML = player1.checkSymbol;
+    _board.push(`${player1.checkSymbol}` + e.target.id);
+
+    console.log(_board);
+  };
+
+  window.addEventListener('touchstart', userMove);
+  window.addEventListener('click', userMove);
+
   return { checkBoard };
 })();
 
-const Players = (name, symbol) => {
-  const player1 = {
-    name: 'Player 1',
-    symbol: 'X',
-  };
-  const player2 = {
-    name: 'Player 2',
-    symbol: 'O',
-  };
-  return { player1, player2 };
+// Handle player data:
+const Player = (name, symbol) => {
+  const checkName = name;
+  const checkSymbol = symbol;
+
+  return { checkName, checkSymbol };
 };
 
-const Controller = (() => {})();
+// Handle game backend
+const Controller = (() => {
+  // let playerTurn = player1;
+  // playerTurn === player1 ? playerTurn === player2 : playerTurn === player1;
+})();
+
+const player1 = Player('Riley', 'X');
+const player2 = Player('Loudon', 'O');
