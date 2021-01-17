df86b27cd3161b1beaffd0c54f9d61681ec52a39
Riley Loudon <rileyloudon@icloud.com>
2021-01-16 20:16:54 -0600

    Stop the game when a winner is found

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
 // Handle all visual changes:
 const Gameboard = (() => {
-  const _board = [];
-  const checkBoard = () => {
-    return _board;
-  };
+  let _board = [];
+  gameOver = false;
+  // const checkBoard = () => {
+  //   return _board;
+  // };
 
   let _playerTurn = '';
 
-  const userMove = (e) => {
-    e.target.classList.contains('game-square')
+  const validMove = (e) => {
+    gameOver
+      ? false
+      : e.target.classList.contains('game-square')
       ? _board.includes(`${player1.playerSymbol}${e.target.id}`) ||
         _board.includes(`${player2.playerSymbol}${e.target.id}`)
         ? false
@@ -23,14 +26,60 @@ const Gameboard = (() => {
     clickedSquare.innerHTML = _playerTurn.playerSymbol;
 
     _board.push(`${_playerTurn.playerSymbol}${e.target.id}`);
-
-    console.log(_board);
+    controller();
   };
 
-  window.addEventListener('touchstart', userMove);
-  window.addEventListener('click', userMove);
+  window.addEventListener('touchstart', validMove);
+  window.addEventListener('click', validMove);
 
-  return { checkBoard };
+  const controller = () => {
+    console.log(_board);
+
+    // End Game
+    (_board.includes('X1') && _board.includes('X2') && _board.includes('X3')) ||
+    (_board.includes('O1') && _board.includes('O2') && _board.includes('O3'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X4') && _board.includes('X5') && _board.includes('X6')) ||
+    (_board.includes('O4') && _board.includes('O5') && _board.includes('O6'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X7') && _board.includes('X8') && _board.includes('X9')) ||
+    (_board.includes('O7') && _board.includes('O8') && _board.includes('O9'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X1') && _board.includes('X4') && _board.includes('X7')) ||
+    (_board.includes('O1') && _board.includes('O4') && _board.includes('O7'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X2') && _board.includes('X5') && _board.includes('X8')) ||
+    (_board.includes('O2') && _board.includes('O5') && _board.includes('O8'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X3') && _board.includes('X6') && _board.includes('X9')) ||
+    (_board.includes('O3') && _board.includes('O6') && _board.includes('O9'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X1') && _board.includes('X5') && _board.includes('X9')) ||
+    (_board.includes('O1') && _board.includes('O5') && _board.includes('O9'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    (_board.includes('X3') && _board.includes('X5') && _board.includes('X7')) ||
+    (_board.includes('O3') && _board.includes('O5') && _board.includes('O7'))
+      ? ((gameOver = true), console.log(`${_playerTurn.playerName} Wins!`))
+      : false;
+
+    _board.length === 9 && !gameOver ? (gameOver = true) : false;
+  };
+
+  // return { checkBoard };
 })();
 
 // Handle player data:
@@ -42,7 +91,4 @@ const Player = (name, symbol) => {
 };
 
 const player1 = Player('Riley', 'X');
-const player2 = Player('Loudon', 'O');
-
-// Handle game backend
-const Controller = (() => {})();
+const player2 = Player('Mary', 'O');
