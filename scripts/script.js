aec6cad4eacb3b56f4a5c334a7fe72b79c3a0f33
Riley Loudon <rileyloudon@icloud.com>
2021-01-24 22:33:40 -0600

    improve computer selection

--- scripts/script.js
+++ scripts/script.js
@@ -46,21 +46,149 @@ const Gameboard = (() => {
 
   // Computer player type
   const displayComputerMove = () => {
+    let computerSelection;
+    let opponent =
+      _playerTurn === player1 ? player2.playerSymbol : player1.playerSymbol;
+
     const allSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let availableSquares = allSquares.filter((square) => {
       return !_board.includes('X' + square) && !_board.includes('O' + square);
     });
 
-    let computerSelection =
-      availableSquares[Math.floor(Math.random() * availableSquares.length)];
+    // check if the computer can win with this turn
+    for (let i = 0; i < availableSquares.length; i++) {
+      _board.push(`${_playerTurn.playerSymbol}${availableSquares[i]}`);
+      if (
+        (_board.includes('X1') &&
+          _board.includes('X2') &&
+          _board.includes('X3')) ||
+        (_board.includes('O1') &&
+          _board.includes('O2') &&
+          _board.includes('O3')) ||
+        (_board.includes('X4') &&
+          _board.includes('X5') &&
+          _board.includes('X6')) ||
+        (_board.includes('O4') &&
+          _board.includes('O5') &&
+          _board.includes('O6')) ||
+        (_board.includes('X7') &&
+          _board.includes('X8') &&
+          _board.includes('X9')) ||
+        (_board.includes('O7') &&
+          _board.includes('O8') &&
+          _board.includes('O9')) ||
+        (_board.includes('X1') &&
+          _board.includes('X4') &&
+          _board.includes('X7')) ||
+        (_board.includes('O1') &&
+          _board.includes('O4') &&
+          _board.includes('O7')) ||
+        (_board.includes('X2') &&
+          _board.includes('X5') &&
+          _board.includes('X8')) ||
+        (_board.includes('O2') &&
+          _board.includes('O5') &&
+          _board.includes('O8')) ||
+        (_board.includes('X3') &&
+          _board.includes('X6') &&
+          _board.includes('X9')) ||
+        (_board.includes('O3') &&
+          _board.includes('O6') &&
+          _board.includes('O9')) ||
+        (_board.includes('X1') &&
+          _board.includes('X5') &&
+          _board.includes('X9')) ||
+        (_board.includes('O1') &&
+          _board.includes('O5') &&
+          _board.includes('O9')) ||
+        (_board.includes('X3') &&
+          _board.includes('X5') &&
+          _board.includes('X7')) ||
+        (_board.includes('O3') &&
+          _board.includes('O5') &&
+          _board.includes('O7'))
+      ) {
+        computerSelection = availableSquares[i];
+        _board.splice(-1, 1);
+        break;
+      } else {
+        _board.splice(-1, 1);
+      }
+    }
 
-    const computerSquare = document.getElementById(`${computerSelection}`);
-    computerSquare.innerHTML = _playerTurn.playerSymbol;
+    // if the computer can't win, check if the opponent can win
+    if (!computerSelection) {
+      for (let i = 0; i < availableSquares.length; i++) {
+        _board.push(`${opponent}${availableSquares[i]}`);
+        if (
+          (_board.includes('X1') &&
+            _board.includes('X2') &&
+            _board.includes('X3')) ||
+          (_board.includes('O1') &&
+            _board.includes('O2') &&
+            _board.includes('O3')) ||
+          (_board.includes('X4') &&
+            _board.includes('X5') &&
+            _board.includes('X6')) ||
+          (_board.includes('O4') &&
+            _board.includes('O5') &&
+            _board.includes('O6')) ||
+          (_board.includes('X7') &&
+            _board.includes('X8') &&
+            _board.includes('X9')) ||
+          (_board.includes('O7') &&
+            _board.includes('O8') &&
+            _board.includes('O9')) ||
+          (_board.includes('X1') &&
+            _board.includes('X4') &&
+            _board.includes('X7')) ||
+          (_board.includes('O1') &&
+            _board.includes('O4') &&
+            _board.includes('O7')) ||
+          (_board.includes('X2') &&
+            _board.includes('X5') &&
+            _board.includes('X8')) ||
+          (_board.includes('O2') &&
+            _board.includes('O5') &&
+            _board.includes('O8')) ||
+          (_board.includes('X3') &&
+            _board.includes('X6') &&
+            _board.includes('X9')) ||
+          (_board.includes('O3') &&
+            _board.includes('O6') &&
+            _board.includes('O9')) ||
+          (_board.includes('X1') &&
+            _board.includes('X5') &&
+            _board.includes('X9')) ||
+          (_board.includes('O1') &&
+            _board.includes('O5') &&
+            _board.includes('O9')) ||
+          (_board.includes('X3') &&
+            _board.includes('X5') &&
+            _board.includes('X7')) ||
+          (_board.includes('O3') &&
+            _board.includes('O5') &&
+            _board.includes('O7'))
+        ) {
+          computerSelection = availableSquares[i];
+          _board.splice(-1, 1);
+          break;
+        } else {
+          _board.splice(-1, 1);
+        }
+      }
+    }
+
+    // If no one can win, pick a random square
+    if (!computerSelection) {
+      computerSelection =
+        availableSquares[Math.floor(Math.random() * availableSquares.length)];
+    }
 
     _board.push(`${_playerTurn.playerSymbol}${computerSelection}`);
 
-    console.log(availableSquares);
-    console.log('Random Number = ' + computerSelection);
+    const computerSquare = document.getElementById(`${computerSelection}`);
+    computerSquare.innerHTML = _playerTurn.playerSymbol;
 
     if (_board.length >= 5) {
       checkEndGame();
@@ -137,6 +265,7 @@ const Gameboard = (() => {
     return { playerName, playerSymbol, playerScore, playerType };
   };
 
+  // Hide modal, start game with player data
   const startGame = () => {
     const modal = document.getElementById('modal');
     const enteredPlayer1Name = document.getElementById('name1').value;
@@ -149,13 +278,13 @@ const Gameboard = (() => {
       (item) => item.checked,
     ).value;
 
-    if (enteredPlayer1Name && enteredPlayer2Name) {
-      modal.classList.add('hide-modal');
-      player1 = Player(enteredPlayer1Name, 'X', 0, player1Type);
-      player2 = Player(enteredPlayer2Name, 'O', 0, player2Type);
-      _playerTurn = player1;
-      player1Type === 'computer' ? setTimeout(displayComputerMove, 250) : false;
-    }
+    // if (enteredPlayer1Name && enteredPlayer2Name) {
+    modal.classList.add('hide-modal');
+    player1 = Player(enteredPlayer1Name, 'X', 0, player1Type);
+    player2 = Player(enteredPlayer2Name, 'O', 0, player2Type);
+    _playerTurn = player1;
+    player1Type === 'computer' ? setTimeout(displayComputerMove, 250) : false;
+    // }
   };
 
   startBtn = document.getElementById('start-btn');
